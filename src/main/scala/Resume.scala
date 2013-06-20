b3dcd74d9fb1768103c2188b02e8b9b9106a3cf5
Josh <joshamosricher@gmail.com>
2013-06-19 20:10:55 -0700

    Started next layer of send/rcv files

--- src/main/scala/Resume.scala
+++ src/main/scala/Resume.scala
@@ -6,9 +6,20 @@ import java.security.MessageDigest
 
 object Chunks {
   val md = MessageDigest.getInstance("SHA")
-  val CHUNK_INF = ":ci(.*):(.*):(.*)".r//nBytes,offset,digest
+
+  
   type Chunk = (Array[Byte],Long,String)//Data,offset,digest
   type ChunkInfo = (Long,Long,String)//nBytes,offset,digest
+  val CHUNK_INF = ":ci(.*):(.*):(.*)".r//nBytes,offset,digest
+  
+  type FileInfo = (Long,Int,Long,String)//nBytes,ChunkSize,offset,signature
+  val FILE_INF = ":fLi(.*):(.*):(.*):(.*)".r//nBytes,ChunkSize,offset,signature
+  
+  val ACC = ":AkC(.*)".r
+  
+  def getSignature(f:RandomAccessFile):String = {
+    return f.length().toString()
+  }
   
   def readChunk(f:RandomAccessFile,nBytes:Int, offset:Long = 0):Chunk = {
     f.seek(offset)
@@ -18,11 +29,51 @@ object Chunks {
     return (outData,offset,new String(md.digest(outData)))
   }
   
-  def getChunkInfo(ch:Chunk):ChunkInfo = {
-    val nBytes = ch._1.length
-    val offset = ch._2
-    val digest = ch._3
-    return (nBytes,offset,digest)
+  def getCmd(cmdSig:Any,is:InputStream):Any = {
+    val in = new BufferedReader(new InputStreamReader(is))
+    in.readLine() match {
+      case ACC(msg) => {
+        return msg
+      }
+      case _ => {
+        throw new IOException("Unknown acc format")
+      }
+    }
+  }
+  
+  def sendAcc(msg:String="AOK",os:OutputStream) = {
+    val out = new PrintWriter(os, true)
+    out.println(":AkC"+msg)
+  }
+  def getAcc(is:InputStream):String = {
+    val in = new BufferedReader(new InputStreamReader(is))
+    in.readLine() match {
+      case ACC(msg) => {
+        return msg
+      }
+      case _ => {
+        throw new IOException("Unknown acc format")
+      }
+    }
+  }
+  
+  def sendFileInfo(f:RandomAccessFile,os:OutputStream,offset:Long = 0,chunkSize:Int = 1024000)={
+    val out = new PrintWriter(os, true)
+    val nBytes = f.length()
+    val sig = getSignature(f)
+    out.println(s":fLi$nBytes:$chunkSize:$offset:$sig")
+  }
+  
+  def getFileInfo(is:InputStream):FileInfo = {
+    val in = new BufferedReader(new InputStreamReader(is))
+    in.readLine() match {
+      case FILE_INF(nBytes,chunkSize,offset,signature) => {
+        return (nBytes.toLong,chunkSize.toInt,offset.toLong,signature)
+      }
+      case _ =>{
+        throw new IOException("Unrecognized File Info Format")
+      }
+    }
   }
   
   def sendChunkInfo(os:OutputStream,ch:Chunk) = {
@@ -43,6 +94,13 @@ object Chunks {
         throw new IOException("Unrecognized Chunk Info Format")
       }
     }
+  }
+
+  def getChunkInfo(ch:Chunk):ChunkInfo = {
+    val nBytes = ch._1.length
+    val offset = ch._2
+    val digest = ch._3
+    return (nBytes,offset,digest)
   }
   
   def sendChunk(out:OutputStream,ch:Chunk,bufSize:Int=2048):Long = {
@@ -85,6 +143,32 @@ object Chunks {
   def saveChunk(f:RandomAccessFile,ch:Chunk) = {
     f.seek(ch._2) //goto offset
     f.write(ch._1) //write the chunk
+  }
+}
+
+/**
+ * Sends chunks in files across RUDP (or any socket). Does SHA hash checking to ensure accurate transfer.
+ * Methods:
+ * 	send(RandomAccessFile) -> Sends a file in chunks, verifying recept
+ *  						  Throws SocketException if connection is interrupted , gives offset so transfer can be renewed
+ * 	
+ */
+class chunkFileSender(s:Socket) {
+  val in = s.getInputStream()
+  val out = s.getOutputStream()
+  def send(resource:String) = {
+    //0: Fix get/set cmd so errors can easily be built in later
+    //1: sendFileInfo(file)
+    //2: getAcc("GotFileInfo")
+    //3: sendAcc("ReadyToSend")
+    //4: getAcc("ReadyToGet")
+    //5: chunkinator = new Chunkinator(myFile)
+    //6: while chunkinator.hasNextChunk:
+    //7: sendChunk(chunkinator.getNextChunk())
+    //8: getAcc("chunkRecieved") | getAcc("chunkError") || SocketError | timeout <-this is why commnads need to be fixed
+    //9: endwhile
+    //10: getAcc("allFileRecieved")
+    //11: return
   }
 }
 
